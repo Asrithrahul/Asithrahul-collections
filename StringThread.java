2f68392ed9b7eb0a74cd2168e9376cf0b0f9eda5
Asrithrahul <83943148+Asrithrahul@users.noreply.github.com>
2021-05-20 15:42:36 +0530

    Create StringThread.java

---
+++ StringThread.java
@@ -0,0 +1,15 @@
+
+public class StringThread extends Thread {
+	String s1;
+	
+	StringThread(String s1)
+	{
+		this.s1=s1;
+	}
+	public synchronized void run()
+	{
+		System.out.println(s1);
+	}
+
+}
+© 2021 GitHub, Inc.
